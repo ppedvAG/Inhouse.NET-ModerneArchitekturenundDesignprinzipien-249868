2e701a321bc58f094187cacf6d746c7f0ac87ef8
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-05-14 15:41:55 +0200

    Clean Architecture Template

---
+++ CleanArchitecture/src/Application/Common/Models/LookupDto.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Common.Models;
+public class LookupDto
+{
+    public int Id { get; init; }
+
+    public string? Title { get; init; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<TodoList, LookupDto>();
+            CreateMap<TodoItem, LookupDto>();
+        }
+    }
+}
---
+++ CleanArchitecture/src/Domain/Common/BaseEvent.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Domain.Common;
+public abstract class BaseEvent : INotification
+{
+}
